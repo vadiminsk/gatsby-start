2102a35d1c33744d6923f68eb40d2f3c01d00009
vadiminsk <vadim@graphicalliance.co.uk>
2021-03-03 12:22:44 +0300

    add styles for components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p>Web developer based in Minsk</p>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>Web developer based in Minsk</p>
+          <Link className={styles.btn} to="/projects">
+            My portfolio projects
+          </Link>
+        </div>
       </section>
     </Layout>
   )
