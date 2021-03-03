c194a561909e1b4e17a73dc5a9da71a00b1d3e57
vadiminsk <vadim@graphicalliance.co.uk>
2021-03-03 11:20:06 +0300

    add new pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>Paragraph text</p>
+      <p>Paragraph text one more</p>
+    </div>
+  )
+}
