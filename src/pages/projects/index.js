023c23b76d9ed0515fa7b27d801893461c32f8d2
vadiminsk <vadiminsk90@gmail.com>
2021-03-10 13:06:07 +0300

    fetch projects data and add styles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,43 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import styles from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <section className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I`ve Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </section>
     </Layout>
   )
 }
+
+// export page query
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
